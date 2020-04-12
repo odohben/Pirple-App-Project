90bc33444dd9ac052816ee694cb33a9f3f20560c
odohben <62214705+odohben@users.noreply.github.com>
2020-04-12 02:33:23 +0100

    Add files via upload

---
+++ song.kt
@@ -0,0 +1,16 @@
+//Attributes of my favourite song
+
+var artist: string = "Sinach"
+val year_Released = "2019"
+var genre: string = "pop"
+val Duration = "6:00"
+var contributing_Artist: string = "Frank"
+val publisher : string = "Loveworld Records"
+
+//Printing varables
+Log.d("MainActivity", "Sinach")
+Log.d("MainActivity", "2019")
+Log.d("MainActivity", "pop")
+Log.d("MainActivity", "6:00")
+Log.d("MainActivity", "Frank")
+Log.d("MainActivity", "Loveworld Records")
